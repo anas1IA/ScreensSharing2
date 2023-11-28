158c5ee27c1f07a12fa2a973acfe610394a15507
Moussa Jamor <moussajamorsup@gmail.com>
2023-11-28 22:17:08 +0100

    add server inter

---
+++ src/server/Server.java
@@ -0,0 +1,37 @@
+package server;
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+public class Server implements ServerInter {
+	
+	ArrayList<E>
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+	@Override
+	public void sendScreenToUser(Socket user, ImageIcon screenImage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendToAllUsers(ImageIcon screenImage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveScreenFromUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
